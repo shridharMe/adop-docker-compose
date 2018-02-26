373d82787a5a29b240c36083b969b786366b012a
Godfree, Dean J <dean.j.godfree@accenture.com>
2018-02-26 11:40:08 +0000

    grafana updates

--- compose/dockprom/grafana/setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Tokenise the ldap file
-envsubst < /etc/grafana/ldap_template.toml > /etc/grafana/adop_ldap.toml
-
-#hoping the dashboards and datasources are already there and grafana started. Lets see.
